aa8483993a254323c1ef28d78352f31cc1cafc3b
Clément Dufour <dufour.clement@gmail.com>
2021-01-10 20:23:28 +0100

    2020 day16 part2

--- 2020/day16.js
+++ 2020/day16.js
@@ -26,12 +26,9 @@ const example = [
 
 const resultPart1 = (puzzle) => {
     const [ranges, yourTicket, nearbyTickets] = extractInformationsFromTicket(puzzle);
-    const invalidFields = _.flatten(nearbyTickets).filter((field) => !isInRange(field, ranges));
+    const invalidFields = _.flatten(nearbyTickets).filter((field) => !isInRanges(field, ranges));
 
     return invalidFields.reduce((acc, field) => acc + field, 0);
-};
-
-const resultPart2 = (puzzle) => {
 };
 
 const extractInformationsFromTicket = (ticket) => {
@@ -70,13 +67,83 @@ const fusionRanges = (ranges) => ranges.reduce((cleanRanges, range) => {
     return cleanRanges;
 }, []);
 
-const isInRange = (field, ranges) => {
+const isInRanges = (field, ranges) => {
     for (let index = 0; index < ranges.length; index += 1) {
-        const [min, max] = ranges[index];
-
-        if (field >= min && field <= max) {
+        if (isInRange(field, ranges[index])) {
             return true;
         }
     }
     return false;
+};
+
+const isInRange = (field, range) => {
+    const [min, max] = range;
+    return field >= min && field <= max;
+};
+
+// PART 2
+
+const resultPart2 = (puzzle) => {
+    const [titles, yourTicket, nearbyTickets] = extractInformationsFromTicketPart2(puzzle);
+    const nearbyValidTickets = nearbyTickets.filter((ticketFields) => ticketFields.every((field) => isInRanges(field, _.flatten(titles.map((title) => title.ranges)))));
+
+    const titlesWithColumnsAvalaibled = getRangesWithColumnAvalaible(titles, nearbyValidTickets);
+    const titlesWithColumnMatch = matchColumns(titlesWithColumnsAvalaibled);
+
+    const res = titlesWithColumnMatch
+        .filter((title) => title.title.startsWith('departure'))
+        .map((title) => yourTicket[title.columnAvalaibled[0]])
+        .reduce((mult, field) => mult * field);
+    return res;
+};
+
+const extractInformationsFromTicketPart2 = (ticket) => {
+    const yourTicketIndex = ticket.indexOf('your ticket:');
+    const yourNearbyTicketIndex = ticket.indexOf('nearby tickets:');
+
+    const rangeLines = ticket.slice(0, yourTicketIndex - 1);
+    const ranges = extractRangePart2(rangeLines);
+
+    const yourticket = ticket.slice(yourTicketIndex + 1, yourTicketIndex + 2)[0].split(',').map(Number);
+    const nearbytickets = ticket.slice(yourNearbyTicketIndex + 1).map((tick) => tick.split(',').map(Number));
+
+    return [ranges, yourticket, nearbytickets];
+};
+
+const extractRangePart2 = (dirtyRange) => dirtyRange.reduce((baseRanges, line) => {
+    const titleIndex = line.indexOf(':');
+    const title = line.slice(0, titleIndex);
+    const lineRanges = line.slice(titleIndex + 1);
+    const ranges = lineRanges.split('or').map((r) => r.trim().split('-').map(Number));
+
+    baseRanges.push({ title, ranges, columnAvalaibled: [...Array(dirtyRange.length).keys()] });
+
+    return baseRanges;
+}, []);
+
+const getRangesWithColumnAvalaible = (titles, tickets) => {
+    tickets.map((ticket) => {
+        ticket.map((field, indexColumn) => {
+            titles.map((title) => title.ranges).map((ranges, indexTitle) => {
+                const isIncludeInRange = isInRanges(field, ranges);
+                if (!isIncludeInRange) {
+                    titles[indexTitle].columnAvalaibled = titles[indexTitle].columnAvalaibled.filter((column) => column !== indexColumn);
+                }
+            });
+        });
+    });
+
+    return titles;
+};
+
+const matchColumns = (titles) => {
+    titles.sort((title1, title2) => title1.columnAvalaibled.length - title2.columnAvalaibled.length);
+    const columnUsed = [];
+    titles.map((title, index, array) => {
+        title.columnAvalaibled = title.columnAvalaibled.filter((c) => !columnUsed.includes(c));
+        if (title.columnAvalaibled.length === 1) {
+            columnUsed.push(title.columnAvalaibled[0]);
+        }
+    });
+    return titles;
 };
