50e2def6646ae0244a904e4955591207e27ddbe7
Clément Dufour <dufour.clement@gmail.com>
2021-01-17 12:05:25 +0100

    2020 day17 part2

--- 2020/day17.js
+++ 2020/day17.js
@@ -8,7 +8,7 @@ const { getData } = require('./input');
     const puzzle = data.data.split('\n').map((line) => line.split(''));
     puzzle.length -= 1;
     // console.log(puzzle);
-    console.log(resultPart1(example));
+    console.log(resultPart1(puzzle));
     console.log(resultPart2(puzzle));
 })();
 
@@ -92,4 +92,93 @@ const getSumActiveNeighbors = (coords, state) => {
 
 // PART 2
 const resultPart2 = (puzzle) => {
+    const pocket = multipleApplyCycleGeneric(puzzle, 4, 6);
+    const numberActiveStates = getNumberActiveStates(pocket);
+    return numberActiveStates;
+};
+
+const multipleApplyCycleGeneric = (pocket, numberDimensions, numberApply) => {
+    let nextPocket = pocket;
+    for (let index = 0; index < numberDimensions - 2; index += 1) {
+        nextPocket = [nextPocket];
+    }
+
+    for (let index = 0; index < numberApply; index += 1) {
+        nextPocket = applyCycleGeneric(nextPocket, numberDimensions);
+    }
+    return nextPocket;
+};
+
+const applyCycleGeneric = (pocket) => {
+    const nextPocket = generatePocketSkeleton(pocket);
+    const fillnextPocket = (pocket, nextPock, coords) => {
+        if (!Array.isArray(nextPock)) {
+            const currentCoords = translateNextCoordsToCurrentCoords(coords);
+            const sumActiveNeighbors = getSumActiveNeighborsGeneric(pocket, currentCoords);
+            const isNextActive = sumActiveNeighbors === 3 || (isActiveStateGeneric(currentCoords, pocket) && sumActiveNeighbors === 2);
+            if (isNextActive) {
+                setState(nextPocket, coords, '#');
+            }
+        } else {
+            for (let index = 0; index < nextPock.length; index += 1) {
+                fillnextPocket(pocket, nextPock[index], [...coords, index]);
+            }
+        }
+    };
+
+    fillnextPocket(pocket, nextPocket, []);
+
+    return nextPocket;
+};
+
+const generatePocketSkeleton = (pocket) => {
+    const length = pocket.length + 2;
+    if (!Array.isArray(pocket[0])) {
+        return Array(length).fill('.');
+    }
+    return Array.from({ length }, () => generatePocketSkeleton(pocket[0]));
+};
+
+const getSumActiveNeighborsGeneric = (pocket, coords, currentCoords = []) => {
+    let sum = 0;
+    if (!Array.isArray(pocket)) {
+        const element = pocket;
+        const isSameCoords = coords.every((coord, index) => coord === currentCoords[index]);
+        const isActiveCase = element === '#';
+        if (!isSameCoords && isActiveCase) {
+            return 1;
+        }
+        return 0;
+    }
+    const coord = coords[currentCoords.length];
+    for (let index = coord - 1; index <= coord + 1; index += 1) {
+        const element = pocket[index];
+        if (!element) {
+            continue;
+        }
+        sum += getSumActiveNeighborsGeneric(pocket[index], coords, [...currentCoords, index]);
+    }
+    return sum;
+};
+
+const getArrayDepth = (value) => (Array.isArray(value) ? 1 + Math.max(...value.map(getArrayDepth)) : 0);
+const setState = (pocket, coords, state) => {
+    const args = coords.reduce((acc, coord) => `${acc}[${coord}]`, '');
+    eval(`pocket${args}=state`);
+};
+
+const isActiveStateGeneric = (coords, pocket) => {
+    let currentCoords = '';
+    for (let index = 0; index < coords.length; index += 1) {
+        const coord = coords[index];
+        currentCoords = `${currentCoords}[${coord}]`;
+        const exist = eval(`pocket${currentCoords}`);
+        if (!exist) {
+            return false;
+        }
+
+        if (index === coords.length - 1) {
+            return eval(`pocket${currentCoords} === "#"`);
+        }
+    }
 };
