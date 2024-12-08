148becf91a6e7689a1692709f47a75b81c78e412
Clément Dufour <dufour.clement@gmail.com>
2024-12-08 22:03:28 +0100

    feat(2024-2)

--- src/2024/2/solutionDay2.ts
+++ src/2024/2/solutionDay2.ts
@@ -14,31 +14,70 @@ import ISolution from '@/common/types/Solution'
 
 export default class ConcretePuzzle extends ISolution {
 
-  public solveFirst(): number {
-    const reports = initPuzzle(this.input)
+  public solveFirst(a = initPuzzle(this.input)): number {
+    const reports = a
 
-    const countSafeReports = reports.reduce((count, levels) => isSafeReport(levels, 3) ? count + 1 : count
-      , 0)
+    const isSafeReport = (levels: number[], maxDifference: number): boolean => {
+      const increasingWithmaxStep = (numbers: number[]) => numbers
+        .every((num, i) => i === 0 || (num > numbers[i - 1] && num - numbers[i - 1] <= maxDifference))
+
+      return increasingWithmaxStep(levels) || increasingWithmaxStep(levels.reverse())
+    }
+
+    const countSafeReports = reports
+      .reduce((count, levels) => isSafeReport(levels, 3) ? count + 1 : count, 0)
 
     return countSafeReports
   }
 
   public solveSecond(): number {
-    // WRITE SOLUTION 2
-    return 0
+    const reports = initPuzzle(this.input)
+
+    const isSafeReport = (levels: number[], maxDifference: number): boolean => {
+      const increasingWithmaxStepAndTolerateBadLevel = (numbers: number[]) => numbers
+        .reduce((acc, num, i) => {
+          const { indexBadLevel, isSafe } = acc
+          if (!isSafe || i === 0) return acc
+
+          if (i === 1) {
+            if (isGoodLevel(num, numbers[i - 1], maxDifference) && isGoodLevel(numbers[i + 1], num, maxDifference)) return acc
+            if (isGoodLevel(numbers[i + 1], num, maxDifference)) return { ...acc, indexBadLevel: 0 }
+            if (isGoodLevel(numbers[i + 1], numbers[i - 1], maxDifference)) return { ...acc, indexBadLevel: 1 }
+
+            if (isGoodLevel(num, numbers[i - 1], maxDifference)) return acc
+            return { ...acc, isSafe: false }
+          }
+
+          if (indexBadLevel === i - 1) {
+            if (isGoodLevel(num, numbers[i - 2], maxDifference)) return acc
+            return { ...acc, isSafe: false }
+          }
+
+          const isGoodCurrentLevel = isGoodLevel(num, numbers[i - 1], maxDifference)
+          if (isGoodCurrentLevel) return acc
+
+          return { isSafe: indexBadLevel === undefined, indexBadLevel: i }
+        }, {
+          indexBadLevel: undefined as number | undefined,
+          isSafe: true
+        })
+        .isSafe
+
+      return increasingWithmaxStepAndTolerateBadLevel(levels) || increasingWithmaxStepAndTolerateBadLevel(levels.reverse())
+    }
+    const countSafeReports = reports.
+      reduce((count, levels) => isSafeReport(levels, 3) ? count + 1 : count, 0)
+
+    return countSafeReports
   }
 
 }
 
 const initPuzzle = (input): number[][] => {
-  const puzzleInput = input.split('\n')
+  const puzzleInput = input.trim().split('\n')
   return puzzleInput
     .map((line) => line.split(' ').map(Number))
 }
 
-const isSafeReport = (numbers: number[], maxDifference: number): boolean => {
-  const increasingWithmaxStep = numbers.every((num, i) => i === 0 || (num > numbers[i - 1] && num - numbers[i - 1] <= maxDifference))
-  const decreasingWithMaxStep = numbers.every((num, i) => i === 0 || (num < numbers[i - 1] && numbers[i - 1] - num <= maxDifference))
-  console.log(increasingWithmaxStep, decreasingWithMaxStep)
-  return increasingWithmaxStep || decreasingWithMaxStep
-}
+const isGoodLevel = (first, second, maxDifference) => (first > second) && ((first - second) <= maxDifference)
+
