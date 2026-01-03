51b8c7f1e3840ff6f11fa4cd47b6080d93091f1a
Clément Dufour <dufour.clement@gmail.com>
2026-01-03 01:11:35 +0100

    feat(2025-1): solution 2

--- src/2025/1/solutionDay1.ts
+++ src/2025/1/solutionDay1.ts
@@ -16,14 +16,12 @@ export default class ConcretePuzzle extends ISolution {
 
   public solveFirst(): string | number {
     const puzzle = this.input.trim().split('\n')
-    console.log(puzzle)
 
     const result = puzzle.reduce((acc, curr) => {
       const isRight = curr.startsWith('R')
       const distance = Number(curr.slice(1))
 
       const newPointer = isRight ? (acc.pointer + distance) % 100 : (acc.pointer + 100 - distance) % 100
-      // console.log(newPointer)
       const newCountZero = newPointer === 0 ? acc.countZero + 1 : acc.countZero
       return { pointer: newPointer, countZero: newCountZero }
 
@@ -36,8 +34,37 @@ export default class ConcretePuzzle extends ISolution {
   }
 
   public solveSecond(): string | number {
-    // WRITE SOLUTION 2
-    return 'solution 2'
+    const puzzle = this.input.trim().split('\n')
+
+    const result = puzzle.reduce(({ pointer: oldPointer, countZero: oldCountZero }, curr) => {
+      const isRight = curr.startsWith('R')
+      const distance = Number(curr.slice(1))
+
+      const newPointer = isRight ? oldPointer + distance : oldPointer - distance
+      const minPointer = Math.min(oldPointer, newPointer)
+      const maxPointer = Math.max(oldPointer, newPointer)
+
+      const firstMultiple = Math.floor(minPointer / 100) * 100 - 100
+      const lastMultiple = Math.ceil(maxPointer / 100) * 100 + 100
+
+      const isCrossingMultiple = (multiple: number) => {
+        return (isRight && oldPointer < multiple && newPointer >= multiple) || (!isRight && oldPointer > multiple && newPointer <= multiple)
+      }
+
+      let revolutions = 0
+      for (let multiple = firstMultiple; multiple < lastMultiple; multiple += 100) {
+        if (isCrossingMultiple(multiple)) {
+          revolutions += 1
+        }
+      }
+
+      return { pointer: newPointer, countZero: oldCountZero + revolutions }
+
+    }, {
+      pointer: 50,
+      countZero: 0
+    })
+    return result.countZero
   }
 
 }
